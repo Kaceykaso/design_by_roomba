5ed528b179985213030e48729eff5b095b15dce3
Kacey Coughlin <kaceycoughlin@mac.com>
2014-05-12 22:32:40 -0700

    Adding more tests

--- python/test.py
+++ python/test.py
@@ -1,15 +1,28 @@
 import serial;
 import create;
 
+# Create the Create!
 robot = create.Create("/dev/ttyUSB0");
 
-robot.toSafeMode();
+"""
+Safe Mode does not allow Create to move why plugged in, run off cliffs, and stops and adjusts when bumped.
+Full Mode ignores all that
+"""
+#robot.toSafeMode();
+robot.toFullMode();
 
-robot.go(6,0);
+# Move forward in millimeters; optional second parameters is speed (cm/s)
+robot.move(6);
+# Stop
 robot.stop();
 
+# Play a C chord
 robot.playSong( [(60,8),(64,8),(67,8),(72.)] );
 
+"""
+Get current position of Create (x,y,theta),
+where theta is the angle the Create is facing.
+"""
 pose = robot.getPose();
 print "My X is %s" % pose[0];
 print "My Y is %s" % pose[1];
