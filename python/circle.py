1d4459118c3f4f37436cd67cbfed5dd01f496729
Kacey Coughlin <kaceycoughlin@mac.com>
2014-05-28 10:31:33 -0700

    Update circle.py

--- python/circle.py
+++ python/circle.py
@@ -18,9 +18,9 @@ line = "On %s I started drawing a TRIANGLE at:\n" % now
 line += "X: %s\n" % pose[0]
 line += "Y: %s\n" % pose[1]
 line += "Theta: %s\n" % pose[2]
-with open('positions.txt','a') as f:
-  f.write(line)
-f.closed
+#with open('positions.txt','a') as f:
+#  f.write(line)
+#f.closed
 
 # Draw circle
 # Circumference of circle with diameter of 12" = 37.5" (95.25cm)
@@ -34,6 +34,6 @@ line = "On %s I started drawing a TRIANGLE at:\n" % new_now
 line += "X: %s\n" % new_pose[0]
 line += "Y: %s\n" % new_pose[1]
 line += "Theta: %s\n" % new_pose[2]
-with open('positions.txt','a') as f:
-  f.write(line)
-f.closed
+#with open('positions.txt','a') as f:
+#  f.write(line)
+#f.closed
