72fc32d8a03d631422966a015dfcddfd1f1df93f
Kacey Coughlin <kaceycoughlin@mac.com>
2014-05-28 10:38:38 -0700

    Update line.py

--- python/line.py
+++ python/line.py
@@ -14,7 +14,7 @@ robot.toFullMode()
 # Record current position, at start of drawing
 pose = robot.getPose()
 now = strftime("%H:%M:%S %m-%d-%Y")
-line = "On %s I started drawing a Line at:\n" % now
+line = "On %s I started drawing a LINE at:\n" % now
 line += "X: %s\n" % pose[0]
 line += "Y: %s\n" % pose[1]
 line += "Theta: %s\n" % pose[2]
@@ -28,7 +28,7 @@ robot.move(305, 30) # 12" equivilent in millimeters, rounded up, 30cm/s
 # Record position now, at end of drawing
 new_pose = robot.getPose()
 new_now = strftime("%H:%M:%S %m-%d-%Y")
-line = "On %s I finished drawing a Line at:\n" % new_now
+line = "On %s I finished drawing a LINE at:\n" % new_now
 line += "X: %s\n" % new_pose[0]
 line += "Y: %s\n" % new_pose[1]
 line += "Theta: %s\n" % new_pose[2]
