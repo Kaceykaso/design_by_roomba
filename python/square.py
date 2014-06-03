ff0919a9837d0049f12404e51710e596f3ef123a
Kacey Coughlin <kaceycoughlin@mac.com>
2014-06-02 23:13:20 -0700

    Update square.py

--- python/square.py
+++ python/square.py
@@ -13,15 +13,15 @@ robot.toFullMode()
 
 while True:
   sensors = robot.sensors(create.LEFT_BUMP, create.RIGHT_BUMP)
-  if sensors[create.LEFT_BUMP] > 0 or sensors[create.RIGHT_BUMP] > 0:
+  if sensors[create.LEFT_BUMP] == 0 or sensors[create.RIGHT_BUMP] == 0:
+    # Draw square
+    robot.move(45, 30) # ~12.5" equivilent in cm, rounded up; at 30cm/s
+    robot.turn(-90, 60) # 90 degrees clockwise, 60 degrees/s
+    robot.move(45, 30)
+    robot.turn(-90, 60)
+    robot.move(45, 30)
+    robot.turn(-90, 60)
+    robot.move(45, 30)
+  else:
     robot.move(-15,30)
     robot.turn(-90,60)
-    break
-  # Draw square
-  robot.move(45, 30) # ~12.5" equivilent in cm, rounded up; at 30cm/s
-  robot.turn(-90, 60) # 90 degrees clockwise, 60 degrees/s
-  robot.move(45, 30)
-  robot.turn(-90, 60)
-  robot.move(45, 30)
-  robot.turn(-90, 60)
-  robot.move(45, 30)
