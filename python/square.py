b92308dc297979ad2a0f6506f3e0dca848604d4a
Kacey Coughlin <kaceycoughlin@mac.com>
2014-06-02 22:51:38 -0700

    Update square.py

--- python/square.py
+++ python/square.py
@@ -12,33 +12,17 @@ robot = create.Create("/dev/ttyUSB0")
 #robot.toFullMode()
 robot.toSafeMode()
 
-# Record current position, at start of drawing
-pose = robot.getPose()
-now = strftime("%H:%M:%S %m-%d-%Y")
-line = "On %s I started drawing a SQUARE at:\n" % now
-line += "X: %s\n" % pose[0]
-line += "Y: %s\n" % pose[1]
-line += "Theta: %s\n" % pose[2]
-#with open('positions.txt','a') as f:
-#  f.write(line)
-#f.closed
+sensors = robot.sensors([create.LEFT_BUMP, create.RIGHT_BUMP]) #Front bumper sesnors
 
-# Draw square
-robot.move(45, 30) # ~12.5" equivilent in cm, rounded up; at 30cm/s
-robot.turn(-90, 60) # 90 degrees clockwise, 60 degrees/s
-robot.move(45, 30)
-robot.turn(-90, 60)
-robot.move(45, 30)
-robot.turn(-90, 60)
-robot.move(45, 30)
-
-# Record position now, at end of drawing
-new_pose = robot.getPose()
-new_now = strftime("%H:%M:%S %m-%d-%Y")
-line = "On %s I finished drawing a SQUARE at:\n" % new_now
-line += "X: %s\n" % new_pose[0]
-line += "Y: %s\n" % new_pose[1]
-line += "Theta: %s\n" % new_pose[2]
-#with open('positions.txt','a') as f:
-#  f.write(line)
-#f.closed
+while not(sensors[create.LEFT_BUMP] == 1 or sensors[create.RIGHT_BUMP] == 1):
+  # Draw square
+  robot.move(45, 30) # ~12.5" equivilent in cm, rounded up; at 30cm/s
+  robot.turn(-90, 60) # 90 degrees clockwise, 60 degrees/s
+  robot.move(45, 30)
+  robot.turn(-90, 60)
+  robot.move(45, 30)
+  robot.turn(-90, 60)
+  robot.move(45, 30)
+else:
+  robot.move(-15,30)
+  robot.turn(-90,60)
