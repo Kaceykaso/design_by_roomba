845b918e7b76b2ed2eae72465a595dd8e665f7ee
Kacey Coughlin <kaceycoughlin@mac.com>
2014-06-07 22:04:20 -0700

    Update demo.py

--- python/demo.py
+++ python/demo.py
@@ -2,6 +2,26 @@
 import serial
 import time
 
+# Serial port
 N = "/dev/ttyUSB0"
-s = serial.Serial(N, 57600, timeout=4)
 
+def ints2str(lst):
+    '''
+    Taking a list of notes/lengths, convert it to a string
+    '''
+    s = ""
+    for i in lst:
+        if i < 0 or i > 255:
+            raise Exception
+        s = s + str(chr(i))
+    return s
+
+# do some initialization magic
+s = serial.Serial(N, 57600, timeout=4)
+# start code
+s.write(ints2str([128]))
+# Full mode
+s.write(ints2str([132]))
+
+# Square
+s.write(ints2str([152, 17, 137, 1, 44, 128, 0, 156, 1, 144, 137, 1, 44, 0, 1, 157, 0, 90, 153]))
