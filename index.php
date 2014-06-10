278f866559310062723c922594ce1a5851bf9f4d
Kacey Coughlin <kaceycoughlin@mac.com>
2014-06-10 08:36:35 -0700

    Uploading new images, updating index/RossBot.php

--- index.php
+++ index.php
@@ -1,85 +1,211 @@
 <?php
-// Check clicks
-if (isset($_POST['btnForward'])) {
-	addForward();
-	addClicked("up");
-}
-if (isset($_POST['btnLeft'])) {
-	addLeft();
-	addClicked("left");
-}
-if (isset($_POST['btnRight'])) {
-	addRight();
-	addClicked("right");
-}
-if (isset($_POST['btnBackward'])) {
-	addBackward();
-	addClicked("down");
-}
+date_default_timezone_set('America/Los_Angeles');
+$file = "conversation.txt";
+// Simple shapes to draw
+$keywords = array("circle","square","triangle", "line", "spiral", "tree");
+// Bob Ross quotes
+$quotes = array("You know me, I gotta put in a big tree.","Here's your bravery test!","Any time ya learn, ya gain.","Haha, and just beat the devil out of it.","Clouds are very, very free.","Happy as we can be.","I like to beat the brush.","Talk to the tree, make friends with it.","We don't make mistakes, we just have happy accidents.","You can do anything you want to do. This is your world.","We want happy paintings. Happy paintings. If you want sad things, watch the news.");
+$quoteCount = count($quotes);
+$circle_warning = 0;
 
-// Read back counts
-function getForward() {
-	return (int)file_get_contents("forward.txt");
-}
-function getLeft() {
-	return (int)file_get_contents("left.txt");
-}
-function getRight() {
-	return (int)file_get_contents("right.txt");
-}
-function getBackward() {
-	return (int)file_get_contents("backward.txt");
-}
-function getLastClicked($index) {
-	if (file_exists("clicked.txt")) {
-		$temp = file_get_contents("clicked.txt");
-		$temp2 = explode("\n", $temp);
-		$all_clicked = array_reverse($temp2);
-		$clicked = array_slice($all_clicked, 0, 6);
-		return $clicked[$index];
+// Check for user input
+if (isset($_POST["chat"])) {
+	// Write convo
+	$thisChat = $_POST["chat"];
+	$userChat = "<div class=\"user\"><p>".$_POST["chat"]."</p></div><div class=\"clear\"></div>";
+	addText($userChat);
+	$bobChat = "";
+	// Make everything lowercase
+	$thisChat = strtolower($thisChat);
+	// Parse user input
+	$temp = explode(" ", $thisChat);
+	// Strip out weird characters
+	$temp = preg_replace('/[.,?!&*_;]+/', '', $temp);
+	// Search for keywords
+	$parsed = array();
+	foreach ($temp as $word) {
+		if (in_array($word, $keywords)) {
+			$parsed[] = $word;
+		}
 	}
+	$total_parsed = count($parsed);
+	
+		
+	if ($total_parsed > 0) {
+		$alt_bob = "painting";
+		$bobChat .= "<div class=\"bob\"><p>Alrighty then, let's make a ";
+		// List all parsed keywords
+		for ($i = 0; $i < $total_parsed; $i++) {
+			$bobChat .= $parsed[$i];
+			if ($i == $total_parsed-1) {
+				$bobChat .= ".";
+			} else {
+				$bobChat .= ", and then a ";
+			}
+		}
+		$bobChat .= "</p></div><div class=\"clear\"></div>";
+		$command = "";
+		// Chain as many commands as needed
+		for ($i = 0; $i < $total_parsed; $i++) {
+			$command .= "python python/".$parsed[$i].".py";
+			if ($parsed[$i] == "line") { addLine(); }
+			if ($parsed[$i] == "spiral") { addSpiral(); }
+			if ($parsed[$i] == "square") { addSquare(); }
+			if ($parsed[$i] == "triangle") { addTriangle(); }
+			// Check if current shape is a circle, follow up with stop command if so
+			if ($parsed[$i] == "circle") {
+				addCircle();
+				sleep(1);
+				$command .= " && python python/stop.py";
+			}
+			// Check if this is the last one, if not, add more
+			if ($i == $total_parsed-1) {
+				$command .= "";
+			} else {
+				$command .= " && ";
+			}
+		}
+		system($command);
+		$thisQuote = rand(0, $quoteCount);
+		
+		// Check number of each shape drawn so far, respond in kind
+		if (getCircle() > 4) {
+			$alt_bob = "upset";
+			$bobChat .= "<div class=\"bob\"><p>I SAID SOMETHING NEW! I'm adding a triangle!</p></div><div class=\"clear\"></div>";
+			addTriangle();
+			$command = "python python/triangle.py";
+			system($command);
+		} else if ((getCircle() > 3) && ($circle_warning == 1)) {
+			$alt_bob = "unsure";
+			$bobChat .= "<div class=\"bob\"><p>That's a lot of circles! Maybe something new next time?</p></div><div class=\"clear\"></div>";
+			$circle_warning++;
+			echo $alt_bob.": ".$circle_warning;
+		} else {
+			$alt_bob = "painting";
+		}
+		
+		if (getLine() > 4) {
+			$bobChat .= "<div class=\"bob\"><p>I SAID SOMETHING NEW! I'm adding a spiral!</p></div><div class=\"clear\"></div>";
+			addSpiral();
+			$command = "python python/spiral.py";
+			system($command);
+		} else if (getLine() > 3) {
+			$bobChat .= "<div class=\"bob\"><p>That's a lot of lines! Maybe something new next time?</p></div><div class=\"clear\"></div>";
+		}
+		
+		if (getSpiral() > 4) {
+			$bobChat .= "<div class=\"bob\"><p>I SAID SOMETHING NEW! I'm adding a circle!</p></div><div class=\"clear\"></div>";
+			addCircle();
+			$command = "python python/circle.py";
+			system($command);
+		} else if (getSpiral() > 3) {
+			$bobChat .= "<div class=\"bob\"><p>That's a lot of spirals! Maybe something new next time?</p></div><div class=\"clear\"></div>";
+		}
+		
+		if (getSquare() > 4) {
+			$bobChat .= "<div class=\"bob\"><p>I SAID SOMETHING NEW! I'm adding a line!</p></div><div class=\"clear\"></div>";
+			addLine();
+			$command = "python python/line.py";
+			system($command);
+		} else if (getSquare() > 3) {
+			$bobChat .= "<div class=\"bob\"><p>That's a lot of squares! Maybe something new next time?</p></div><div class=\"clear\"></div>";
+		}
+		
+		if (getTriangle() > 4) {
+			$bobChat .= "<div class=\"bob\"><p>I SAID SOMETHING NEW! I'm adding a square!</p></div><div class=\"clear\"></div>";
+			addSquare();
+			$command = "python python/sqare.py";
+			system($command);
+		} else if (getTriangle() > 3) {
+			$bobChat .= "<div class=\"bob\"><p>That's a lot of triangles! Maybe something new next time?</p></div><div class=\"clear\"></div>";
+		}
+		
+		// Other responses
+		$alt_bob = "";
+		if (in_array("tree", $temp)) {$bobChat .= "<div class=\"bob\"><p>I really love trees, great choice!</p></div><div class=\"clear\"></div>";}
+		$bobChat .= "<div class=\"bob\"><p>".$quotes[$thisQuote]."</p></div><div class=\"clear\"></div>";
+	} else {
+		if (in_array("imperial", $temp)) {
+			$alt_bob = "stormtrooper";
+			$bobChat = "<div class=\"bob\"><p>Lord Vader has no interest in your puny demands.</p></div><div class=\"clear\"></div>";
+			system('python python/imperial_march.py');
+		} else if (in_array("test", $temp)) {
+			$alt_bob = "";
+			$bobChat = "<div class=\"bob\"><p>Running test script.</p></div><div class=\"clear\"></div>";
+			system('python python/test2.py');
+		} else if (in_array("love", $temp)) {
+			$alt_bob = "love";
+			$bobChat = "<div class=\"bob\"><p>What do I love? I love trees!</p><p>Happy little trees.</p></div><div class=\"clear\"></div>";
+		} else {
+			$bobChat = "<div class=\"bob\"><p>I can't draw that. How about a happy little tree?</p></div><div class=\"clear\"></div>";
+			$alt_bob = "";
+		}
+
+	}
+	addText($bobChat);
+	$lastTime = date('h:i:s A');
 }
 
-// Record counts
-function addForward() {
-	$forwardCount = getForward() + 1;
-	file_put_contents("forward.txt", $forwardCount);
-}
-function addLeft() {
-	$leftCount = getLeft() + 1;
-	file_put_contents("left.txt", $leftCount);
-}
-function addRight() {
-	$rightCount = getRight() + 1;
-	file_put_contents("right.txt", $rightCount);
-}
-function addBackward() {
-	$backCount = getBackward() + 1;
-	file_put_contents("backward.txt", $backCount);
-}
-// Record all
-function addClicked($direction) {
-	$lastClicked = $direction."\n";
-	file_put_contents("clicked.txt", $lastClicked, FILE_APPEND | LOCK_EX);
+
+// Check for keywords
+
+// Translate keywords to commands
+
+	// Give feedback that command is being executed
+	
+	// Give feedback when finished
+	
+// Give feedback on piece as a whole so far
+
+
+
+// If its been a while, chat a bit, say something Bob Ross-ish
+function checkTime() {
+	$timeStamp = date('h:i:s A');
+	return $timeStamp;
 }
 
-// Reset all counts
-if (isset($_POST['reset'])) {
-	 file_put_contents("forward.txt", 0);
-	 file_put_contents("left.txt", 0);
-	 file_put_contents("right.txt", 0);
-	 file_put_contents("backward.txt", 0);
-	 file_put_contents("clicked.txt", "");
+// Reset
+if ($_POST['reset']) {
+	if (file_exists($file)) { unlink($file); }
+	file_put_contents("circle.txt", 0);
+	file_put_contents("line.txt", 0);
+	file_put_contents("spiral.txt", 0);
+	file_put_contents("square.txt", 0);
+	file_put_contents("triangle.txt", 0);
 }
-
-// Get max,min values
-$up = getForward();
-$down = getBackward();
-$left = getLeft();
-$right = getRight();
-$values = array($up,$down,$left,$right);
-$first = max($values);
-$last = min($values);
+// Conversation
+function addText($text) {
+	$lastLine = $text."\n";
+	file_put_contents("conversation.txt", $lastLine, FILE_APPEND | LOCK_EX);
+}
+// Get counts 
+function getCircle() { return (int)file_get_contents("circle.txt"); }
+function getLine() { return (int)file_get_contents("line.txt"); }
+function getSpiral() { return (int)file_get_contents("spiral.txt"); }
+function getSquare() { return (int)file_get_contents("square.txt"); }
+function getTriangle() { return (int)file_get_contents("triangle.txt"); }
+// Records counts
+function addCircle() {
+	$circleCount = getCircle() + 1;
+	file_put_contents("circle.txt", $circleCount);
+}
+function addLine() {
+	$lineCount = getLine() + 1;
+	file_put_contents("line.txt", $lineCount);
+}
+function addSpiral() {
+	$spiralCount = getSpiral() + 1;
+	file_put_contents("spiral.txt", $spiralCount);
+}
+function addSquare() {
+	$squareCount = getSquare() + 1;
+	file_put_contents("square.txt", $squareCount);
+}
+function addTriangle() {
+	$triangleCount = getTriangle() + 1;
+	file_put_contents("triangle.txt", $triangleCount);
+}
 
 ?>
 
@@ -107,6 +233,17 @@ $last = min($values);
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <script type="text/javascript">
+	function saveScrollPositions(theForm) {
+	
+		if(theForm) {
+		var scrolly = typeof window.pageYOffset != 'undefined' ? window.pageYOffset : document.documentElement.scrollTop;
+		var scrollx = typeof window.pageXOffset != 'undefined' ? window.pageXOffset : document.documentElement.scrollLeft;
+		theForm.scrollx.value = scrollx;
+		theForm.scrolly.value = scrolly;
+		}
+	}
+	</script>
   </head>
 
   <body>
@@ -126,10 +263,10 @@ $last = min($values);
 			    <!-- Everything else -->
 			    <div class="collapse navbar-collapse">
 			      <ul class="nav navbar-nav pull-right">
-			      	<li><a href="rossbot.php">Bob Ross Mode</a></li>
+			      	<li><a href="index.php">Command Mode</a></li>
 			        <li><a href="about.html">About</a></li>
 			        <li><a href="https://github.com/Kaceykaso/design_by_roomba/tree/gh-pages">Fork Me!</a></li>
-			       
+			        
 			      </ul>
 			      
 			    </div><!-- end .collapse -->
@@ -137,101 +274,46 @@ $last = min($values);
 		</nav>
 
 		<div class="container-fluid">
-          <div class="row main">
+          <div class="row">
+          	<div class="picture-container">
+          		<div class="bob <?php echo $alt_bob; ?>"></div>
+          	</div>
+          </div>
           
-          	<div class="col-xs-8 col-sm-6 col-md-4">
-          		<div class="graph">
-          			
-		          	<div class="graph-container">
-			          	<!-- Labels -->
-		          		<div class="bar-container graph-labels">
-		          			<p>100%</p>
-		          			<p>50%</p>
-		          			<p>0%</p>
-		          		</div>
-		          		<!-- Right -->
-		          		<div class="bar-container commandRight">
-		          			<div class="bar
-		          			<?php if ($left == $first) {echo " first";}
-		          				else if ($left == $last) {echo " last";} ?>
-		          			" style="height: <?php echo getLeft(); ?>%;">
-		          			</div>
-		          			<?php echo getLeft(); ?>
-		          		</div>
-		          		<!-- Up -->
-		          		<div class="bar-container commandUp">
-		          			<div class="bar
-		          			<?php if ($up == $first) {echo " first";}
-		          				else if ($up == $last) {echo " last";} ?>
-		          			" style="height: <?php echo getForward(); ?>%;">
-		          			</div>
-		          			<?php echo getForward(); ?>
-		          		</div>
-		          		<!-- Down -->
-		          		<div class="bar-container commandDown">
-		          			<div class="bar
-		          			<?php if ($down == $first) {echo " first";}
-		          				else if ($down == $last) {echo " last";} ?>
-		          			" style="height: <?php echo getBackward(); ?>%;">
-		          			</div>
-		          			<?php echo getBackward(); ?>
-		          		</div>
-		          		<!-- Left -->
-		          		<div class="bar-container commandLeft">
-		          			<div class="bar
-		          			<?php if ($right == $first) {echo " first";}
-		          				else if ($right == $last) {echo " last";} ?>
-		          			" style="height: <?php echo getRight(); ?>%;">
-		          			</div>
-		          			<?php echo getRight(); ?>
-		          		</div>
-		          	</div><!-- end .graph-container -->
-		          	<div class="bar-labels">
-		          			<i></i>
-		          			<i class="fa fa-arrow-left <?php if (getLastClicked(1) == "left") {echo "justClicked";}?>"></i>
-		          			<i class="fa fa-arrow-up <?php if (getLastClicked(1) == "up") {echo "justClicked";}?>"></i>
-		          			<i class="fa fa-arrow-down <?php if (getLastClicked(1) == "down") {echo "justClicked";}?>"></i>
-		          			<i class="fa fa-arrow-right <?php if (getLastClicked(1) == "right") {echo "justClicked";}?>"></i>
-		          		</div>
-          		</div><!-- end .graph -->
-          	</div><!-- end .col -->
+          <div class="row">
+	      	<div class="chat-container">
+	          	<div class="chat-transcript" id="chat-transcript">
+	          			<?php 
+				        	if (file_exists($file)) {
+					        	$read = file_get_contents($file);
+								$chats = explode("\n", $read);
+								foreach ($chats as $talk) {
+									echo $talk;
+								}
+				        	} else {
+								echo "<div class=\"bob\"><p>Hi there! I'm glad to see you today! What shall we make today?</p></div><div class=\"clear\"></div>";
+								$thisQuote = rand(0, $quoteCount-1);
+								echo "<div class=\"bob\"><p>".$quotes[$thisQuote]."</p></div><div class=\"clear\"></div>";
+							}
+				         ?>
+	          	</div>
+	          		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" role="form" name="form" onsubmit="return saveScrollPositions(this);">
+						<input type="hidden" name="scrollx" id="scrollx" value="0" />
+						<input type="hidden" name="scrolly" id="scrolly" value="0" />
+	          			<input type="text" class="chat" name="chat" placeholder="Draw a circle!" value="">
+	          			<input type="submit" name="submit" class="btn btn-primary btn-xs" value="Go!">
+	          			<input type="submit" name="reset" class="btn btn-default btn-xs" value="Reset">
+	          		</form>
+	         </div>
+         </div>
           	
-	          <div class="col-xs-8 col-sm-6 col-md-4 middle">
-	            <h3 class="heading">Which way<br><small>my master?</small></h3>
-	            <br>
-	            <form role="form" action="" method="post">
-	            	<div class="form-group">
-	            		<button type="submit" name="btnForward" class="btn btn-lg btn-success" value="forward"><i class="fa fa-arrow-up"></i></button>
-	            	</div>
-	            	<div class="form-group">
-	            		<button type="submit" name="btnLeft" class="btn btn-lg btn-primary" value="left"><i class="fa fa-arrow-left"></i></button>
-	            		<button type="submit" name="btnRight" class="btn btn-lg btn-info" value="right"><i class="fa fa-arrow-right"></i></button>
-	            	</div>
-	            	<div class="form-group">
-	            		<button type="submit" name="btnBackward" class="btn btn-lg btn-warning" value="backward"><i class="fa fa-arrow-down"></i></button>
-	            	</div>
-	            	<button type="submit" name="reset" class="btn btn-lg"><i class="fa fa-refresh"></i>  Reset</button>
-	            </form>
-	          </div><!-- end .col -->
-	          
-	          <div class="col-xs-8 col-sm-12 col-md-4 feedback">
-	          <?php if (getLastClicked(1) == "") { ?>
-		          <i class="fa fa-spinner fa-spin fa-5x"></i><br>
-		          <p class="lead text-center">
-		          Waiting for commands:
-	          	</p>
-	          <?Php } else { ?>
-		          <i class="fa fa-arrow-<?php echo getLastClicked(5); ?> fa-5x"></i>
-		          <i class="fa fa-arrow-<?php echo getLastClicked(4); ?> fa-5x"></i>
-		          <i class="fa fa-arrow-<?php echo getLastClicked(3); ?> fa-5x"></i>
-		          <i class="fa fa-arrow-<?php echo getLastClicked(2); ?> fa-5x"></i>
-		          <i class="fa fa-arrow-<?php echo getLastClicked(1); ?> fa-5x"></i>
-		      <?php } ?>
+         <div class="row">
+	          <div class="instructions">
+	          		RossBot is an artist, but he loves to collaborate! Give RossBot suggestions or directions as to what he should incorporate into his work. You can say things like "Draw a <a class="tag">circle</a>s", "Add some <a class="tag">spiral</a>s", "Give it a <a class="tag">triangle</a>", or even "Make a <a class="tag">happy little tree</a>". RossBot likes happy things. <a class="tag">Zigzag</a>'s make him sad.
 	          </div>
-	          
-          </div><!-- end .row -->
+          </div>
 
-          <footer class="navbar-fixed-bottom">
+          <footer class="">
               <p>A <a href="http://www.kaceycoughlin.com/">Kacey Coughlin</a> joint.</p>
           </footer>
 
@@ -243,6 +325,49 @@ $last = min($values);
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/prefixfree.min.js"></script>
+    <script>
+    	// Add tags to chat when clicked
+    	$(document).on("click", "a.tag", function() { 
+    
+	    // add selected class
+	    $(this).toggleClass('selected');
+	    
+	    // make a collection of tags
+	    var newTag = [];
+	    $('a.tag.selected').each(function(){
+	                
+	        // add value;
+	        newTag.push($(this).text());
+	    });
+	    $('.chat').val(newTag.join(', '));
+	    $('.chat').focus(); // Put focus on chat
+	    
+	    return false;  
+	});
+	// Submit form when Enter key is hit
+	$("input").keypress(function(event) {
+	    if (event.which == 13) {
+	        event.preventDefault();
+	        $("form").submit();
+	    }
+	});
+    </script>
+    <?php
+	$scrollx = 0;
+	$scrolly = 0;
+	if(!empty($_REQUEST['scrollx'])) {
+		$scrollx = $_REQUEST['scrollx'];
+	}
+	
+	if(!empty($_REQUEST['scrolly'])) {
+		$scrolly = $_REQUEST['scrolly'];
+	}
+	?>
+	<script type="text/javascript">
+	window.scrollTo(<?php echo "$scrollx" ?>, <?php echo "$scrolly" ?>);
+	var objDiv = document.getElementById("chat-transcript");
+	objDiv.scrollTop = objDiv.scrollHeight;
+	</script>
   </body>
 </html>
 
