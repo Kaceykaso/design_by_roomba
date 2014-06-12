fd08a0bd9e26cf36950429a7b6d7d55204993020
Kacey Coughlin <kaceycoughlin@mac.com>
2014-06-12 07:41:56 -0700

    Create hello.py

---
+++ python/hello.py
@@ -0,0 +1,16 @@
+# Saying hello with song
+import Create
+
+
+
+
+
+# Song
+robot.playSong( [(60,8),(64,8),(67,8),(72,8)] ) # C chord
+robot.playSong([(62,5)]) # D
+robot.playSong([(64,5)]) # E
+robot.playSong([(65,5)]) # F
+robot.playSong([(67,5)]) # G
+robot.playSong([(69,5)]) # A
+robot.playSong([(71,5)]) # B
+robot.playSong( [(60,8),(64,8),(67,8),(72,8)] ) # C chord
