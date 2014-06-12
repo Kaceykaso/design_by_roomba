c3de1f627ba6095e7942d93474ddd7c7cf3620b7
Kacey Coughlin <kaceycoughlin@mac.com>
2014-06-12 16:05:36 -0700

    Update circle.py

--- python/circle.py
+++ python/circle.py
@@ -11,29 +11,7 @@ from time import strftime
 robot = create.Create("/dev/ttyUSB0")
 robot.toFullMode()
 
-# Record current position, at start of drawing
-pose = robot.getPose()
-now = strftime("%H:%M:%S %m-%d-%Y")
-line = "On %s I started drawing a CIRCLE at:\n" % now
-line += "X: %s\n" % pose[0]
-line += "Y: %s\n" % pose[1]
-line += "Theta: %s\n" % pose[2]
-#with open('positions.txt','a') as f:
-#  f.write(line)
-#f.closed
-
 # Draw circle
 # Circumference of circle with diameter of 12.5" ~ 2pi(22.5) = 141
 robot.go(141,-360)
 # Will turn forever, in PHP call stop script after ~1s
-
-# Record position now, at end of drawing
-new_pose = robot.getPose()
-new_now = strftime("%H:%M:%S %m-%d-%Y")
-line = "On %s I finished drawing a CIRCLE at:\n" % new_now
-line += "X: %s\n" % new_pose[0]
-line += "Y: %s\n" % new_pose[1]
-line += "Theta: %s\n" % new_pose[2]
-#with open('positions.txt','a') as f:
-#  f.write(line)
-#f.closed
